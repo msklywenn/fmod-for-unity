ad9baa2b0caedb65352e7875e5a04fd6f2be69b0
Daniel Borges <msk@lywenn.eu.org>
2023-09-12 10:26:52 +0200

    crash fix

--- Assets/Plugins/FMOD/src/Editor/EventManager.cs
+++ Assets/Plugins/FMOD/src/Editor/EventManager.cs
@@ -20,7 +20,7 @@ namespace FMODUnity
 
         private const string CacheAssetName = "FMODStudioCache";
         public static string CacheAssetFullName =>
-            $"{RuntimeUtils.PluginBasePath}/Cache/Editor/{CacheAssetName}.asset";
+            $"Assets/Plugins/FMOD/Cache/Editor/{CacheAssetName}.asset";
         private static EventCache eventCache;
 
         private const string StringBankExtension = "strings.bank";
